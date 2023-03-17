bd6a02c2e4abef7854ef761e52e6d80a2ded4279
Omoadonibetty <omoadonibetty@gmail.com>
2023-03-17 12:54:06 -0700

    Print Smile

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - main function
- *
- * Return: always 0
+ * main - prints the lowercase alphabet in reverse,
+ * followed by a new line
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-        char i;
+	char ch;
 
-	for (i = 'z' ; i >= 'a' ; i--)
-		putchar(i);
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
 	putchar('\n');
 	return (0);
 }
+
