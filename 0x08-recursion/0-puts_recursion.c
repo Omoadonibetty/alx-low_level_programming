f4254223e32efe4632bff1b2c49e98703f5fb0f5
Omoadonibetty <omoadonibetty@gmail.com>
2023-04-05 15:25:03 -0700

    PUTIN RECURSION

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion - function puts();
+ * @i: input
+ * Return: Always 0 (Success)
+ */
+void _puts_recursion(char *i)
+{
+	if (*i)
+	{
+		_putchar(*i);
+		_puts_recursion(i + 1);
+	}
+
+	else
+		_putchar('\n');
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+Recursion task in C Alx
