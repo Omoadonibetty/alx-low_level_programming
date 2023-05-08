86b2485aaa4b5e523201d5e544a721aaac5b4a12
Omoadonibetty <omoadonibetty@gmail.com>
2023-05-08 16:49:00 -0700

    Dropped

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -27,7 +27,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	san = open(filename, O_WRONLY | O_APPEND);
 	don = write(san, text_content, len);
 
-	if (san == -1 || dan == -1)
+	if (san == -1 || don == -1)
 		return (-1);
 
 	close(san);
