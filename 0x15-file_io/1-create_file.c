b5aea1caf20c29ac9570f761c31ec7df71f3b516
Omoadonibetty <omoadonibetty@gmail.com>
2023-05-08 16:28:16 -0700

    Boksoon ans killing

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd; 
 `	int san;
-lap = 0;
+	lap = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -28,7 +28,7 @@ lap = 0;
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	san = write(fd, text_content, lap);
 
-	if (san == -1 || fd == -1)
+	if (fd == -1 || sam == -1)
 		return (-1);
 
 	close(fd);
